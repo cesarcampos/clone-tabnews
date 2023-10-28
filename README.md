ec65e82ea5e8578f57c830eb03e4d976494d81c3
Cesar Campos <cesarcampos@gmail.com>
2023-10-28 02:47:35 +0000

    adciona scripts e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-
 function Home() {
-    return <h1>Bruno... esse curso que você me indicou. é massa... Curtindo.</h1>;
+  return <h1>Bruno... esse curso que você me indicou. é massa... Curtindo.</h1>;
 }
 
 export default Home;
