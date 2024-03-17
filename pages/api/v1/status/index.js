960851042442039be12fe2fcb1841390741cdd0a
Cesar Campos <cesarcampos@gmail.com>
2024-03-17 13:44:07 +0000

    add `database.js` abnd `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("select 1+1;");
+  console.log(result.rows);
   res.status(200).json({ chave: "Tudo OK com o status." });
 }
 
