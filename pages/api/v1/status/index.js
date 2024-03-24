610688a444254b2af9222c2c5be65801e89919b7
Cesar Campos <cesarcampos@gmail.com>
2024-03-24 03:05:29 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("select 1+1;");
-  console.log(result.rows);
-  res.status(200).json({ chave: "Tudo OK com o status." });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  const dbMaxConnResult = await database.query("SHOW max_connections;");
+  const dbMaxConnValue = dbMaxConnResult.rows[0].max_connections;
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbOpenedConnResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnValue = dbOpenedConnResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnValue),
+        opened_connections: dbOpenedConnValue,
+      },
+    },
+  });
 }
 
 export default status;
